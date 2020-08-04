4a956afb505800437d9ce162071ecd97d3b131c4
Mike Archer <mlarcher531@gmail.com>
2020-08-04 18:01:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_english_meaning(file_path, jap_emoticon)
   eng_lib.each do |meaning, emotes|
     #binding.pry
     emotes.each do |language, emo,|
-      binding.pry
+      #binding.pry
       if language == jap_emoticon
         p meaning
   end
