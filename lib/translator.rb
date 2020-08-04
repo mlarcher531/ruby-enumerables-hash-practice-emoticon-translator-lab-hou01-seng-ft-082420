73231b7108bec2d06b9a94937b56c08c7b00a564
Mike Archer <mlarcher531@gmail.com>
2020-08-04 18:05:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_english_meaning(file_path, jap_emoticon)
       if emo == jap_emoticon
         p meaning
   end
-  binding.pry
+  #binding.pry
 end
   end
   "Sorry, that emoticon was not found"
