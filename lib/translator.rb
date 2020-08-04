a9d34bdd1501f9c76483a12af463d4409fe7eba6
Mike Archer <mlarcher531@gmail.com>
2020-08-04 17:17:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
 table = {}
   emoticons.each do |key, value|
-    table[key] ||= {}
+    table[key] = {}
     binding.pry
     table[key][:english] = value[0]
     table[key][:japanese] = value[1]
