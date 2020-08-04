288512d1af953c925adeb1393d121633c421702e
Mike Archer <mlarcher531@gmail.com>
2020-08-04 16:54:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,9 @@ require'pry'
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
+  emoticons.each do |key, value, language|
       binding.pry
+    end
 end
 
 def get_japanese_emoticon
